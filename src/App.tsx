2dfcc7031b914ec188dff886c909e9695ead3ccd
Kizy Matzenbacher <kizymatzen@gmail.com>
2023-03-30 00:42:37 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import Header from './Header';
-import TeamList from './TeamList';
+import teams from './data/CollegeBasketballTeams.json';
 import './App.css';
 
-function App() {
+interface Team {
+  school: string;
+  name: string;
+  abbrev: string;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Header(): JSX.Element {
+  return (
+    <header>
+      <h1>College Basketball Teams</h1>
+      <p>Welcome to our NCAA basketball teams website. Here, you can find information on all the basketball teams in the NCAA.</p>
+    </header>
+  );
+}
+
+function TeamCard(props: Team): JSX.Element {
+  return (
+    <div className="team-card">
+      <h2>{props.school}</h2>
+      <p>Mascot: {props.name}</p>
+      <p>Abbreviation: {props.abbrev}</p>
+      <p>Location: {props.city}, {props.state}</p>
+    </div>
+  );
+}
+
+function TeamList(): JSX.Element {
+  return (
+    <div>
+      {teams.teams.map((team: Team, index: number) => (
+        <TeamCard key={index} {...team} />
+      ))}
+    </div>
+  );
+}
+
+
+function App(): JSX.Element {
   return (
     <div>
       <Header />
